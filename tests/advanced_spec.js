bb60cafde83e252afa35f00c67ac2f7628147043
airman5573 <rpf5573@kakao.com>
2021-11-22 16:20:32 +0900

    Add : advanced test cases

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -3,7 +3,10 @@
 const { isValidXML } = require("../src");
 
 describe("advanced validator test", () => {
-  it("FILL ME", () => {
-    // FILL ME
+  it("should return true for an xml with a multi nodes", () => {
+    expect(isValidXML("<note><to>yoon</to><from>kim</from><heading>Hi yoon</heading><body>Do you want to play table tennis with me this week?</body></note>")).toBeTruthy();
+  });
+  it("should return true for an xml with a tag which has whitespaces", () => {
+    expect(isValidXML("<mess age></message>")).toBeFalsy();
   });
 });
